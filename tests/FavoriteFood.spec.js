01b922474f305106a2b8bf5a40a4a63adcb66d02
Kevin McCormack <wittyman37@yahoo.com>
2016-03-23 09:36:04 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -3,9 +3,18 @@ describe('Favorite Food Filter', function () {
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		filterFF = $filter('favoriteFood');
 	}));
 
-	
+	it('should filter objects by favoriteFood', function () {
+		var people = [
+			{name: 'Jose', favoriteFood: 'pizza'},
+			{name: 'Sally', favoriteFood: 'ice cream'}
+		];
+
+		expect(filterFF(people, 'pizza')).toEqual([people[0]]);
+		expect(filterFF(people, 'ice cream')).toEqual([people[1]]);
+		expect(filterFF(people, 'soda')).toEqual([]);
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -3,8 +3,13 @@ describe('removeAllVowels Filter', function () {
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
 
+	it('should remove all the vowels from a string', function () {
+		expect(removeAllVowels('Panda')).toEqual('Pnd');
+		expect(removeAllVowels('food')).toEqual('fd');
+		expect(removeAllVowels('This thing works!')).toEqual('Ths thng wrks!');
+	});
 });
